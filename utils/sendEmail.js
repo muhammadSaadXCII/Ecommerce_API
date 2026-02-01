81dfe0fc4b1ca86ecadbd776096fa4370eebfcbc
Muhammad Saad <saadmughal532@gmail.com>
2026-02-01 13:24:43 +0500

    Fixed Email Errors

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,12 +3,15 @@ const nodemailer = require("nodemailer");
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    port: Number(process.env.EMAIL_PORT),
     secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
   });
 
   await transporter.sendMail({
