df656b1c280198331a31aa18fd5e407de856b816
Muhammad Saad <saadmughal532@gmail.com>
2026-02-01 13:04:37 +0500

    Fixed Auth Errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,23 +14,25 @@ const generateEmailToken = () => {
 exports.registerUser = async (req, res) => {
     try {
         const { name, email, password, role } = req.body;
-        const user = await User.findOne({ email });
-        if (user) {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
             return res.status(400).json({ message: "User already exists." });
         }
+
+        const emailToken = generateEmailToken();
         const newUser = new User({
-            name: name,
-            email: email,
-            password: password,
-            role: role,
-            emailToken: generateEmailToken(),
+            name,
+            email,
+            password,
+            role,
+            emailToken,
             emailTokenExpires: Date.now() + 1000 * 60 * 60
         });
         await newUser.save();
 
         const verifyUrl = `${process.env.BASE_URL}/api/auth/verify-email?token=${emailToken}`;
         await sendEmail({
-            to: user.email,
+            to: email,
             subject: "Verify your email",
             html: `
               <h2>Email Verification</h2>
@@ -54,12 +56,16 @@ exports.loginUser = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
 
         if (user && isMatch) {
-            res.status(201).json({
-                userId: user.id,
-                userEmail: user.email,
-                userRole: user.role,
-                userToken: generateJWTToken(user.id, user.email, user.role)
-            });
+            if (user.isVerified) {
+                res.status(201).json({
+                    userId: user.id,
+                    userEmail: user.email,
+                    userRole: user.role,
+                    userToken: generateJWTToken(user.id, user.email, user.role)
+                });
+            } else {
+                return res.status(403).json({ message: "Please verify your email first" });
+            }
         } else {
             res.status(401).json({ message: "Invalid Credentials" });
         }
